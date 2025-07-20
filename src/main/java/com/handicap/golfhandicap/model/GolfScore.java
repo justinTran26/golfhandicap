01a138570440b26d0d434c85cf3010ad601c0811
justinTran26 <163887032+justinTran26@users.noreply.github.com>
2025-07-19 20:02:41 -0400

    changed model for differentials

--- src/main/java/com/handicap/golfhandicap/model/GolfScore.java
+++ src/main/java/com/handicap/golfhandicap/model/GolfScore.java
@@ -13,24 +13,29 @@ public class GolfScore {
     private LocalDate date;
     private int score;
 
+    private int slopeRating;
+    private double courseRating;
+    private int coursePar;
+    private double differential;
+
     // Getters and setters
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
 
-    public LocalDate getDate() {
-        return date;
-    }
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
 
-    public int getScore() {
-        return score;
-    }
+    public int getSlopeRating() { return slopeRating; }
+    public void setSlopeRating(int slopeRating) { this.slopeRating = slopeRating; }
 
-    public void setScore(int score) {
-        this.score = score;
-    }
+    public double getCourseRating() { return courseRating; }
+    public void setCourseRating(double courseRating) { this.courseRating = courseRating; }
+
+    public int getCoursePar() { return coursePar; }
+    public void setCoursePar(int coursePar) { this.coursePar = coursePar; }
+
+    public double getDifferential() { return differential; }
+    public void setDifferential(double differential) { this.differential = differential; }
 }
